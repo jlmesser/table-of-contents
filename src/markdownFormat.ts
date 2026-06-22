cb4ae57e164637305f1b44a4b5ea0ce4763d4c0d
jenny <jennylmesser@gmail.com>
2026-06-22 06:37:38 +0100

    Add support for reference links

--- src/markdownFormat.ts
+++ src/markdownFormat.ts
@@ -1,32 +1,38 @@
 import {PdfCompatibilityMode} from "./util";
 
 //todo review all regexes to ensure they're correct and reduce duplication if necessary
+const INNER_REFERENCE_LINK_REGEX = /\[(.*)]\[(.*)]/g;
+const BLOCK_LINK_REGEX = /(\[\[)#\^([A-Za-z0-9-]+]])/; //[[#^2395bd]]
+
 
 export function cleanMarkdown(stripHeadingForLink: string, pdfSettings: number, heading: string) {
-	let text = replaceOuterLinks(heading.trim());
-	let cleanText = hasInnerLinks(text) ? stripHeadingForLink : replaceEscapeCodeBlocks(text);
+	const text = replaceOuterLinks(heading.trim());
+	const cleanText = hasInnerLinks(text) ? stripHeadingForLink : replaceEscapeCodeBlocks(text);
 
-	return createHeadingWikilink(cleanText, pdfSettings, heading);
+	const s = createHeadingWikilink(cleanText, pdfSettings, heading);
+	console.log("cleaned link: " + s);
+	return s;
 }
 
 function replaceOuterLinks(text: string) {
+	//todo make BLOCK_LINK_REGEX actually check for existing block links in cache instead of regex?
+
 	const WIKILINK_REGEX = /^\[\[.+\|(.+)]]$/; //[[file#section|display text]] -> display text
 	const MARKDOWN_LINK_REGEX = /^\[([^[\]()]+)]\([^[\]()]+\)$/; //[text](url) -> text
-	const BLOCK_LINK_REGEX = /(\[\[)#\^([A-Za-z0-9-]+]])/;
-
+	const REFERENCE_LINK_REGEX = /^\[(.*)]\[(.*)]$/; //[like][this]
 	return text
-		.replace(BLOCK_LINK_REGEX, "$1$2")
+		.replace(BLOCK_LINK_REGEX, "$1 $2")
 		.replace(WIKILINK_REGEX, "$1")
-		.replace(MARKDOWN_LINK_REGEX, "$1");
+		.replace(MARKDOWN_LINK_REGEX, "$1")
+		.replace(REFERENCE_LINK_REGEX, "$1 $2");
 }
 
 function hasInnerLinks(text: string) {
-	const innerMarkdownLinkRegex = /\[([^[\]()]+)]\([^[\]()]+\)/;
-	const innerWikiLinkRegex = /\[\[([^|\]]+)\|([^\]]+)]]/g;
-	return innerMarkdownLinkRegex.test(text) || innerWikiLinkRegex.test(text);
+	const INNER_LINK_REGEX = /\[([^[\]()]+)]\([^[\]()]+\)/g;
+	const INNER_WIKILINK_REGEX = /\[\[([^|\]]+)\|([^\]]+)]]/g;
+	return INNER_LINK_REGEX.test(text) || INNER_WIKILINK_REGEX.test(text) || INNER_REFERENCE_LINK_REGEX.test(text);
 }
 
-// Match inline code blocks, escaped characters, or raw Markdown styling
 function replaceEscapeCodeBlocks(text: string) {
 	const ESCAPE_AND_CODE_REGEX = /`(.+)`|\\([\^*_~`=[\]])|([\^*_~`=[\]])/g;
 	return text
@@ -42,26 +48,55 @@ function createHeadingWikilink(cleanText: string, pdfSetting: number, heading: s
 	const CLOSE_LINK = " ]]";
 	const LINK_RENAME = "|";
 
+	console.log("cleanText " + cleanText);
+	console.log("heading " + heading);
+
 	if (cleanText == heading) {
 		return OPEN_LINK + heading + CLOSE_LINK;
 	}
 
-	const headingText = heading.replace(/^\[{2}|]{2}$/g, '');
+	const SQUARE_BRACKETS_REGEX = /^\[{2}|]{2}$/g;
+	const headingText = heading.replace(SQUARE_BRACKETS_REGEX, '');
 
-	if (isBlockLink(cleanText, headingText)) {
-		return `${OPEN_LINK}${cleanText}${CLOSE_LINK}`;
+	console.log("headingText " + headingText);
+
+	let htmlLink = "";
+
+	const lastWord = cleanText.trim().split(/\s+/).pop() || '';
+	if (headingText.includes(`^${lastWord}`) ) {
+
+		const internalBlockLinkedTitleName = /(?:.*\|\^?(.*)\]{2}\^.*)|(?:^#\^(.*$))/;
+		const s = headingText.replace(internalBlockLinkedTitleName, "$1$2");
+
+		console.log("headingText " + s);
+
+		return getHtmlLink(s, s, BLOCK_LINK_REGEX.test(heading) ? 0 : pdfSetting, `${OPEN_LINK}${cleanText}${LINK_RENAME}${s}${CLOSE_LINK}`);
+		// return getHtmlLink(headingText, cleanText, pdfSetting) + " | " + getHtmlLink( "#^" + lastWord, cleanText, pdfSetting) + " | " + `${OPEN_LINK}${cleanText}${CLOSE_LINK}`;
+
+		// const ct2 = cleanText.replace(" " + lastWord, "");
+		// const lw2 = "#^" + lastWord;
+		// return
+		// getHtmlLink( lw2, ct2, pdfSetting)
+		// 	+ " | " + `${OPEN_LINK}${cleanText}${CLOSE_LINK}`
+		// 	+ " | " + `${OPEN_LINK}${headingText}${LINK_RENAME}${cleanText}${CLOSE_LINK}`
+		// 	+ " | " + `${OPEN_LINK}${lw2}${LINK_RENAME}${ct2}${CLOSE_LINK}` ;
 	}
 
-	const isRenamedWikilink = headingText.endsWith(LINK_RENAME + cleanText);
-	if (isRenamedWikilink) {
-		return getHtmlLink(headingText, cleanText, pdfSetting);
+
+	const isRenamedWikilink = headingText.includes(LINK_RENAME + cleanText);
+	const useObsidianHtml = headingText.includes(`\^`) || heading.includes("][");
+	if (isRenamedWikilink || INNER_REFERENCE_LINK_REGEX.test(headingText) || useObsidianHtml ) {
+		console.log("isRenamedWikilink");
+		return getHtmlLink(headingText, cleanText, useObsidianHtml ? 0 : pdfSetting); // both HTML work in PDF for [^1], 1 HTML obsidian none PDF for [^2395bd]
 	}
 
-	const doLinkRename = heading.match(/^[^\d\w].*$/);
-	if (doLinkRename) {
-		return `${OPEN_LINK}${headingText}${LINK_RENAME}${cleanText}${CLOSE_LINK}`;
+	const FIRST_CHAR_SPECIAL_REGEX = /^[^a-zA-Z0-9].*$/;
+	if (heading.match(FIRST_CHAR_SPECIAL_REGEX)) {
+		console.log("isFIRST_CHAR_SPECIAL_REGEX");
+		return `${OPEN_LINK}${headingText}${LINK_RENAME}${cleanText}${CLOSE_LINK}`;// + " | " + htmlLink; //this for [^1]
 	} else {
-		return `${OPEN_LINK}${cleanText}${CLOSE_LINK}`;
+		console.log("else");
+		return `${OPEN_LINK}${cleanText}${CLOSE_LINK}`;// + " | " + htmlLink; //this for [[#^2395bd]] block link
 	}
 
 }
@@ -71,9 +106,13 @@ function isBlockLink(cleanText: string, headingText: string) {
 	return headingText.includes(`^${lastWord}`);
 }
 
-function getHtmlLink(headingText: string, cleanText: string, pdfSetting: number) {
-	let obsidianLink = "<a class=\"internal-link\" href=\"#" + headingText + "\" data-href=\"#" + headingText + "\">" + cleanText + "</a>";
-	let pdfLink = "<a href=\"#" + cleanText + "\">" + cleanText + "</a>";
+function getHtmlLink(headingText: string, cleanText: string, pdfSetting: number, obsidianOverride?: string) {
+	const obsidianHtml = `<a class="internal-link" href="#${headingText}" data-href="#${headingText}">${cleanText}</a>`;
+	// const decodedText = decodeURIComponent(headingText); //breaks remove caret
+	// console.log("cleanText " + cleanText);
+	// console.log("headingText " + headingText);
+	const pdfLink = `<a href="#${cleanText}">${cleanText}</a>`;
+	const obsidianLink = obsidianOverride ? obsidianOverride : obsidianHtml;
 	switch (pdfSetting) {
 		case PdfCompatibilityMode.OBSIDIAN.valueOf():
 			return obsidianLink;
@@ -81,6 +120,8 @@ function getHtmlLink(headingText: string, cleanText: string, pdfSetting: number)
 			return pdfLink;
 		case PdfCompatibilityMode.BOTH.valueOf():
 		default:
-			return obsidianLink + " | " + pdfLink;
+			return `${obsidianLink} | ${pdfLink}`;
 	}
 }
+
+
--- tests/markdownFormat.test.ts
+++ tests/markdownFormat.test.ts
@@ -6,30 +6,34 @@ import {PdfCompatibilityMode} from "../src/util";
 
 //todo handle headings with multiple links (of internal/wiki links, 2 of each and both)
 //todo test long headings?
-//todo test other characters (special, emojis, look for charset to test with)
+//todo test other characters (special, emojis, look for charsets to test with)
 
 describe("clean link display text", () => {
 	it.each`
-		heading                                                                      			| stripHeadingForLink                     			| pdfSettings                   		| expected
-		${'**Hello**'}                                                               | ${'Hello'}                              | ${PdfCompatibilityMode.BOTH}  | ${'[[#**Hello**|Hello ]]'}
-		${'`code`'}                                                                  | ${'code'}                               | ${PdfCompatibilityMode.BOTH}  | ${'[[#`code`|code ]]'}
-		${'==highlight=='}                                                           | ${'highlight'}                          | ${PdfCompatibilityMode.BOTH}  | ${'[[#==highlight==|highlight ]]'}
-		${'***bold and italic***'}                                                   | ${'bold and italic'}                    | ${PdfCompatibilityMode.BOTH}  | ${'[[#***bold and italic***|bold and italic ]]'}
-		${'~~strikethrough~~'}                                                       | ${'strikethrough'}                      | ${PdfCompatibilityMode.BOTH}  | ${'[[#~~strikethrough~~|strikethrough ]]'}
-		${'[[ToC bugs|othername1]]'}                                                 | ${'othername1'}                         | ${PdfCompatibilityMode.BOTH}  | ${'<a class="internal-link" href="#ToC bugs|othername1" data-href="#ToC bugs|othername1">othername1</a> | <a href="#othername1">othername1</a>'}
-		${'[[ToC bugs|othername1]]'}                                                 | ${'othername1'}                         | ${PdfCompatibilityMode.OBSIDIAN}| ${'<a class="internal-link" href="#ToC bugs|othername1" data-href="#ToC bugs|othername1">othername1</a>'}
-		${'[[ToC bugs|othername1]]'}                                                 | ${'othername1'}                         | ${PdfCompatibilityMode.PDF}   | ${'<a href="#othername1">othername1</a>'}
-		${'[display link text](xyz.com)'}                                            | ${'display link text'}                  | ${PdfCompatibilityMode.BOTH}  | ${'[[#[display link text](xyz.com)|display link text ]]'}
-		${'`keep chars *_~`=[] in code block`'}                                      | ${'keep chars *_~`=[] in code block'}   | ${PdfCompatibilityMode.BOTH}  | ${'[[#`keep chars *_~`=[] in code block`|keep chars *_~`=[] in code block ]]'}
-		${'keep escaped chars \\* \\_ \\~ \\` \\= \\[ \\]'}                           | ${'keep escaped chars * _ ~ ` = [ ]'}   | ${PdfCompatibilityMode.BOTH}  | ${'[[#keep escaped chars * _ ~ ` = [ ] ]]'}
-		${'keep (brackets)'}                                                         | ${'keep (brackets)'}                    | ${PdfCompatibilityMode.BOTH}  | ${'[[#keep (brackets) ]]'}
-		${'[[display internal link file name]]'}                                     | ${'display internal link file name'}    | ${PdfCompatibilityMode.BOTH}  | ${'[[#display internal link file name|display internal link file name ]]'}
-		${'[othername2](ToC%20bugs)'}                                                | ${'othername2'}                         | ${PdfCompatibilityMode.BOTH}  | ${'[[#[othername2](ToC%20bugs)|othername2 ]]'}
-		${'[[ToC bugs|remove^2395bd]]^2395bd'}                                       | ${'ToC bugs remove 2395bd 2395bd'}      | ${PdfCompatibilityMode.BOTH}  | ${'[[#ToC bugs remove 2395bd 2395bd ]]'}
-		${'[[#^2395bd]]'}                                                            | ${'2395bd'}                             | ${PdfCompatibilityMode.BOTH}  | ${'[[#2395bd ]]'}
-		${'some text [[ToC bugs|2muchtxt]] more text'}                               | ${'some text ToC bugs 2muchtxt more text'}| ${PdfCompatibilityMode.BOTH}  | ${'[[#some text ToC bugs 2muchtxt more text ]]'}
-		${'some text [[ToC bugs|3muchtxt]] more text [[ToC bugs|4muchtxt]] more text'}| ${'some text ToC bugs 3muchtxt more text ToC bugs 4muchtxt more text'}| ${PdfCompatibilityMode.BOTH}| ${'[[#some text ToC bugs 3muchtxt more text ToC bugs 4muchtxt more text ]]'}
-		${'things n stuff [othername3](ToC%20bugs) things n stuff'}                 | ${'things n stuff othername3 ToC 20bugs things n stuff'}| ${PdfCompatibilityMode.BOTH}| ${'[[#things n stuff othername3 ToC 20bugs things n stuff ]]'}
+		heading                                                                  | stripHeadingForLink                     							| pdfSettings                   		    | expected
+		${'**Hello**'}                                                 | ${'Hello'}                              				| ${PdfCompatibilityMode.BOTH}    | ${'[[#**Hello**|Hello ]]'}
+		${'`code`'}                                                    | ${'code'}                               				| ${PdfCompatibilityMode.BOTH}    | ${'[[#`code`|code ]]'}
+		${'==highlight=='}                                             | ${'highlight'}                          				| ${PdfCompatibilityMode.BOTH}    | ${'[[#==highlight==|highlight ]]'}
+		${'***bold and italic***'}                                     | ${'bold and italic'}                    				| ${PdfCompatibilityMode.BOTH}    | ${'[[#***bold and italic***|bold and italic ]]'}
+		${'~~strikethrough~~'}                                         | ${'strikethrough'}                      				| ${PdfCompatibilityMode.BOTH}    | ${'[[#~~strikethrough~~|strikethrough ]]'}
+		${'[[ToC bugs|othername1]]'}                                   | ${'othername1'}                         				| ${PdfCompatibilityMode.BOTH}    | ${'<a class="internal-link" href="#ToC bugs|othername1" data-href="#ToC bugs|othername1">othername1</a> | <a href="#othername1">othername1</a>'}
+		${'[[ToC bugs|othername1]]'}                                   | ${'othername1'}                         				| ${PdfCompatibilityMode.OBSIDIAN}| ${'<a class="internal-link" href="#ToC bugs|othername1" data-href="#ToC bugs|othername1">othername1</a>'}
+		${'[[ToC bugs|othername1]]'}                                   | ${'othername1'}                         				| ${PdfCompatibilityMode.PDF}     | ${'<a href="#othername1">othername1</a>'}
+		${'[display link text](xyz.com)'}                              | ${'display link text'}                  				| ${PdfCompatibilityMode.BOTH}    | ${'[[#[display link text](xyz.com)|display link text ]]'}
+		${'`keep chars *_~`=[] in code block`'}                        | ${'keep chars *_~`=[] in code block'}   				| ${PdfCompatibilityMode.BOTH}    | ${'[[#`keep chars *_~`=[] in code block`|keep chars *_~`=[] in code block ]]'}
+		${'keep escaped chars \\* \\_ \\~ \\` \\= \\[ \\]'}            | ${'keep escaped chars * _ ~ ` = [ ]'}   				| ${PdfCompatibilityMode.BOTH}    | ${'[[#keep escaped chars * _ ~ ` = [ ] ]]'}
+		${'keep (brackets)'}                                           | ${'keep (brackets)'}                    				| ${PdfCompatibilityMode.BOTH}    | ${'[[#keep (brackets) ]]'}
+		${'[[display internal link file name]]'}                       | ${'display internal link file name'}    				| ${PdfCompatibilityMode.BOTH}    | ${'[[#display internal link file name|display internal link file name ]]'}
+		${'[othername2](ToC%20bugs)'}                                  | ${'othername2'}                         				| ${PdfCompatibilityMode.BOTH}    | ${'[[#[othername2](ToC%20bugs)|othername2 ]]'}
+		${'[[ToC bugs|remove caret]]^2395bd'}                         | ${'ToC bugs remove caret 2395bd'}      				| ${PdfCompatibilityMode.BOTH}    | ${'[[#ToC bugs remove caret 2395bd|remove caret ]] | <a href=\"#remove caret\">remove caret</a>'}
+		${'[[#^2395bd]]'}                                              | ${'2395bd'}                             				| ${PdfCompatibilityMode.BOTH}    | ${'[[#2395bd ]] | <a href=\"#2395bd\">2395bd</a>'}
+		${'some text [[ToC bugs|2muchtxt]] more text'}                 | ${'some text ToC bugs 2muchtxt more text'} 			| ${PdfCompatibilityMode.BOTH}    | ${'[[#some text ToC bugs 2muchtxt more text ]]'}
+		${'some [[ToC bugs|3muchtxt]] text [[ToC bugs|4muchtxt]] more'}| ${'some ToC bugs 3muchtxt text ToC bugs 4muchtxt more'}| ${PdfCompatibilityMode.BOTH}   | ${'[[#some ToC bugs 3muchtxt text ToC bugs 4muchtxt more ]]'}
+		${'things [othername3](ToC%20bugs) stuff'}                 	 | ${'things othername3 ToC 20bugs stuff'} 				| ${PdfCompatibilityMode.BOTH}    | ${'[[#things othername3 ToC 20bugs stuff ]]'} <!-- //todo fix ToC 20bugs stuff from ToC%20bugs-->
+		${'before footnote [^1] after footnote'}                       | ${'before footnote 1 after footnote'}                  | ${PdfCompatibilityMode.BOTH}    | ${'<a class=\"internal-link\" href=\"#before footnote [^1] after footnote\" data-href=\"#before footnote [^1] after footnote\">before footnote 1 after footnote</a>'} <-- [[#before footnote 1 after footnote ]] -->
+		${'[^1]'}                       								 | ${'1'}                              				    | ${PdfCompatibilityMode.BOTH}    | ${'[[#1|[^1] ]] | <a href=\"#[^1]\">[^1]</a>'}
+		${'This is a text with a [shortcut link][my-id]'}              | ${'This is a text with a shortcut link my-id'}         | ${PdfCompatibilityMode.BOTH}    | ${'[[#This is a text with a shortcut link my-id ]]'}
+		${'[this][my-id]'}                       					     | ${'this my-id'}                              			| ${PdfCompatibilityMode.BOTH}    | ${'[[#this my-id ]]'}
 	`("for heading '$heading' create link '$expected'", ({ heading, stripHeadingForLink, pdfSettings, expected }) => {
 		expect(
 			cleanMarkdown(
